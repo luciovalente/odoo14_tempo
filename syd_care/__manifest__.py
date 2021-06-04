42e0ed81eda70ada1ba0172e70c20f4e3faf8d57
Lucio <lucio.valente@infosons.com>
2021-06-04 13:59:24 +0200

    syd care

---
+++ syd_care/__manifest__.py
@@ -0,0 +1,21 @@
+# Copyright 2019 Camptocamp SA
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Bit2Win",
+    "version": "14.0.1.0.0",
+    "category": "Bit2Win",
+    "author": "Rapsodoo Italia",
+    
+    "website": "https://www.rapsodoo.com",
+    "depends": ["web",'web_enterprise'],
+    "data": [
+             "security/security.xml",
+         "security/ir.model.access.csv",
+        "views/views.xml",
+        "views/templates.xml",
+    ],
+    'qweb': [
+        "static/src/xml/qweb_templates.xml",
+    ],
+    "installable": True,
+}
